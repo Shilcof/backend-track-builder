ffe7d40d8a0a712ea83bad7f457c12ee418d88b4
Shilcof <robert.shilcof@gmail.com>
2021-02-15 11:14:02 +0000

    migrates database

--- db/migrate/20210215110035_create_segments.rb
+++ db/migrate/20210215110035_create_segments.rb
@@ -2,7 +2,7 @@ class CreateSegments < ActiveRecord::Migration[6.1]
   def change
     create_table :segments do |t|
       t.integer :position
-      t.integer :type
+      t.integer :segment_type
       t.belongs_to :track, null: false, foreign_key: true
 
       t.timestamps

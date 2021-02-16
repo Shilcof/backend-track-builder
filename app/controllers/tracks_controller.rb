896c43d00fff3f14e5770ae06645e94ac4d4c0c0
Shilcof <robert.shilcof@gmail.com>
2021-02-16 11:20:10 +0000

    updates json responses to include track ids

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,14 +1,12 @@
 class TracksController < ApplicationController
     def index
         tracks = Track.all
-        render json: tracks, only: [:name], include: [
-            segments: {only: [:position, :segment_type]}
-        ]
+        render json: tracks, only: [:id, :name]
     end
 
     def show
         track = Track.find(params[:id])
-        render json: track, only: [:name], include: [
+        render json: track, only: [:id, :name], include: [
             segments: {only: [:position, :segment_type]}
         ]
     end

5fc82b51c86ce63aa4b252bd30adf7cb24cfd449
Shilcof <robert.shilcof@gmail.com>
2021-02-17 19:46:35 +0000

    adds no symbol validator module and validates track

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,6 +1,10 @@
 class Track < ApplicationRecord
     has_many :segments, dependent: :destroy
 
+    validates :name, presence: true
+    validates :name, :creator, no_symbols: true
+    validate :valid_track
+
     def segments_attributes=(attributes)
         segments.each{|s| s.destroy}
         attributes.each { |pos, type|
@@ -10,9 +14,16 @@ class Track < ApplicationRecord
         }
     end
 
+    def valid_track
+        if !valid_track?
+            errors.add(:segments, "must obey the track constructor rules")
+        end
+    end
+
     def valid_track?
         path = {}
         current = segments.find_by(position: 41)
+        return false if !current
         from = 40
         path[current.position] = current.segment_type
         while current.position != 40

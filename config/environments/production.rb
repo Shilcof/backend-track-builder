d4d9b39bcbbca2d022f33c779e62d729b3db5ab6
Shilcof <robert.shilcof@gmail.com>
2021-02-20 18:30:59 +0000

    alters cors url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,5 +112,5 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   # Action cable allows all requests
-  config.action_cable.allowed_request_origins = ['https://track-builder.netlify.app/']
+  config.action_cable.allowed_request_origins = ['https://track-builder.netlify.app']
 end

92de6d092b6d172cdbd00b34b6ae649da52491c6
Shilcof <robert.shilcof@gmail.com>
2021-02-20 18:15:03 +0000

    adds cors frontend site

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://track-builder.netlify.app/'
 
-    resource '*',
+    resource 'https://track-builder.netlify.app/',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
